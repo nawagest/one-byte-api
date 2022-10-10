abc61fe2517d29cafcd4b1979b43aeb83c993fd5
nawagest <nathanaeldt25@gmail.com>
2022-10-10 09:45:05 -0400

    bug fixes

--- index.js
+++ index.js
@@ -3,37 +3,41 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const Recipe = require('./models/RecipeModel');
+require('dotenv').config();
 
 mongoose.connect(process.env.MONGOURI);
 
-const corsOptions = {
-    origin: "https://my-cheesy-app.nawagest.repl.co",
-    optionsSuccessStatus: 200
-}
+// const corsOptions = {
+//     origin: "https://my-cheesy-app.nawagest.repl.co",
+//     optionsSuccessStatus: 200
+// }
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', async (req, res) => {
-  res.send('This is the api for my cheesy app')
+app.get('/', (req, res) => {
+    res.send('This is the api for my cheesy app')
 });
 
-app.get('/recipes', cors(corsOptions), (req, res) => {
-  res.json(Recipe.find({}));
+app.get('/recipes', (req, res) => {
+    Recipe.find({}, (recipes) => {
+        console.log(recipes);
+        res.sendStatus(200);
+    });
 });
 
-app.post('/recipes', cors(corsOptions), (req, res) => {
+app.post('/recipes', (req, res) => {
     const data = req.body;
-    console.log(data);
     const recipe = new Recipe(data);
     recipe.save((err) => {
         if(err) {
-            res.status(404).send('Something went wrong');
+            res.status(404).json({msg: 'Something went wrong'});
         }
     })
 });
 
-app.get('/recipes/:id', cors(corsOptions), (req, res) => {
+app.get('/recipes/:id', (req, res) => {
     const { id } = req.params
     Recipe.find({ recipeID: id }, (value) => {
         if(value) res.json(value)
@@ -41,6 +45,6 @@ app.get('/recipes/:id', cors(corsOptions), (req, res) => {
     });
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log('api running')
 });
